e73b739f7dd6c6eaf8d97a68fbd40822894af799
Chih Liang Tseng <wanyu840126@gmail.com>
2015-12-21 19:59:17 +0800

    First

---
+++ src/d0248384.java
@@ -0,0 +1,13 @@
+
+public class d0248384 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("d0248384");
+
+	}
+
+}
